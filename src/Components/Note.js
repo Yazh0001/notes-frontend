3694ea0d4fedcdcffa77b777f68e01e1e1b386eb
Yazh0001 <Yasir.azhari2000@gmail.com>
2024-02-11 20:29:08 +0400

    Working edit feature

--- src/Components/Note.js
+++ src/Components/Note.js
@@ -1,15 +1,32 @@
-import React from 'react'
-import { useState } from 'react'
+import axios from 'axios'
+import { useState} from 'react'
 
 export default function Note({noteId, title, content, deleteNote}) {
+    
+
+    const [editmode, setEditmode] = useState(false)
+
+    const [titleState, setTitleState] = useState(title)
+    const [contentState, setContentState] = useState(content)
+
     const handleDelete = ()=>{
-        deleteNote(noteId)
+      deleteNote(noteId)
+  }
+
+    const handleEdit = ()=>{
+      setEditmode(!editmode)
+    }
+
+    const handleSave = ()=>{
+        axios.put('https://localhost:7241/api/Notes/' + noteId, {"title": titleState, "content": contentState})
+        setEditmode(!editmode)
     }
   return (
     <div>
-        <div>{title}</div>
-        <div>{content}</div>
+        {editmode ? <input onChange={(e)=>setTitleState(e.target.value)} defaultValue={titleState}></input> : <div>{titleState}</div>}
+        {editmode ? <input onChange={(e)=>setContentState(e.target.value)} defaultValue={contentState}></input> : <div>{contentState}</div>}
         <button onClick={handleDelete}>Delete</button>
+        {editmode ? <button onClick={handleSave}>save</button> : <button onClick={handleEdit}>edit</button>}
     </div>
   )
 }
--- src/Components/NoteBox.js
+++ src/Components/NoteBox.js
@@ -4,9 +4,7 @@ import Note from './Note'
 import axios from 'axios'
 
 export default function NoteBox() {
-    const [notes, setNotes] = useState([
-        {id: "Idtest1", title: "Untitled 001", content: "This is the first note"},
-        {id: "Idtest2", title: "Untitled 002", content: "This is the second note"}])
+    const [notes, setNotes] = useState([])
 
     useEffect(()=>{
         axios.get('https://localhost:7241/api/Notes').then((res)=>{
@@ -20,14 +18,14 @@ export default function NoteBox() {
             setNotes(notes.filter((note)=>note.noteId !== id))
         }
 
-        const addNote = ()=>{
+        const addNote = (note)=>{
             var newid = Math.random().toString()
             setNotes([...notes, {noteId: newid, title: "Untitled", content: "Empty"}])
         }
 
   return (
     <div>
-        {notes.map((n)=><Note noteId={n.noteId} title={n.title} content={n.content} deleteNote={deleteNote}/>)}
+        {notes.map((n)=><Note noteId={n.noteId} title={n.title} content={n.content} deleteNote={deleteNote} />)}
         <button onClick={addNote}>New Note</button>
     </div>
   )
